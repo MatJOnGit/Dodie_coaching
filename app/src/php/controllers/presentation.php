9ac7e6304e8210f5f7bab3cb5bd5af09abbe2715
Mathieu JOURDAN <ma.jourdan@hotmail.fr>
2022-07-08 18:40:46 +0200

    Adding MVC architecture. Adding fontawesome and font families

---
+++ app/src/php/controllers/presentation.php
@@ -0,0 +1,4 @@
+<?php
+
+echo $twig->render('templates/header.twig');
+echo $twig->render('member/presentation.twig');
--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
 
-echo 'Hello !';
+require_once './app/vendor/autoload.php';
+$loader = new \Twig\Loader\FilesystemLoader('./app/src/php/views');
+$twig = new \Twig\Environment($loader, [
+    'cache' => false,
+    'debug' => true
+]);
+$twig->addExtension(new \Twig\Extension\DebugExtension());
+
+require ('./app/src/php/controllers/presentation.php');

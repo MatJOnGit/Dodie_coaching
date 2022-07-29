29c51832990140ceb0d379c340878b5493f5f9a5
Mathieu JOURDAN <ma.jourdan@hotmail.fr>
2022-07-29 20:47:33 +0200

    Removing description data from getProgramsList query in ShowcaseController. Lightning programs shading in programsList and programDetails pages. Regrouping all showcase pages style in one showcase.css file. Removed splitter in programDetails page. Moving text in form tiny button into a paragraph. Adding style to tiny-button paragraph.

--- app/src/php/controllers/ShowcaseController.php
+++ app/src/php/controllers/ShowcaseController.php
@@ -4,16 +4,8 @@ require ('app/src/php/controllers/MainController.php');
 require('app/src/php/model/ProgramManager.php');
 
 class ShowcaseController {
-    public $introPagesStyles = [
-        'pages/showcase-intro',
-        'components/header',
-        'components/static-menu',
-        'components/internal-links',
-        'components/footer',
-        'themes/showcase-section'
-    ];
-    public $programsPagesStyles = [
-        'pages/showcase-programs',
+    public $showcasePagesStyles = [
+        'pages/showcase',
         'components/header',
         'components/static-menu',
         'components/internal-links',
@@ -34,14 +26,14 @@ class ShowcaseController {
     }
 
     public function renderPresentationPage($twig) {
-        echo $twig->render('templates/head.twig', ['stylePaths' => $this->introPagesStyles]);
+        echo $twig->render('templates/head.twig', ['stylePaths' => $this->showcasePagesStyles]);
         echo $twig->render('templates/header.twig', ['requestedPage'=> 'presentation']);
         echo $twig->render('showcase/presentation.twig');
         echo $twig->render('templates/footer.twig');
     }
 
     public function renderCoachingPage($twig) {
-        echo $twig->render('templates/head.twig', ['stylePaths' => $this->introPagesStyles]);
+        echo $twig->render('templates/head.twig', ['stylePaths' => $this->showcasePagesStyles]);
         echo $twig->render('templates/header.twig', ['requestedPage'=> 'coaching']);
         echo $twig->render('showcase/coaching.twig');
         echo $twig->render('templates/footer.twig');
@@ -50,7 +42,7 @@ class ShowcaseController {
     public function renderProgramsListPage($twig) {
         $programsList = $this->requestProgramList();
 
-        echo $twig->render('templates/head.twig', ['stylePaths' => $this->programsPagesStyles]);
+        echo $twig->render('templates/head.twig', ['stylePaths' => $this->showcasePagesStyles]);
         echo $twig->render('templates/header.twig', ['requestedPage' => 'programslist']);
         echo $twig->render('showcase/programslist.twig', ['programsList' => $programsList]);
         echo $twig->render('templates/footer.twig');
@@ -59,7 +51,7 @@ class ShowcaseController {
     public function renderProgramDetailsPage($twig) {
         $programsDetails = $this->requestProgramDetails();
         
-        echo $twig->render('templates/head.twig', ['stylePaths' => $this->programsPagesStyles]);
+        echo $twig->render('templates/head.twig', ['stylePaths' => $this->showcasePagesStyles]);
         echo $twig->render('templates/header.twig', ['requestedPage'=> 'programdetails']);
         echo $twig->render('showcase/programdetails.twig', ['requestedPage' => 'programdetails', 'program' => $programsDetails]);
         echo $twig->render('templates/footer.twig');

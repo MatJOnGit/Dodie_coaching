03c724fbd12d8da1fe665c9d38449755c9949d7f
Mathieu JOURDAN <ma.jourdan@hotmail.fr>
2022-12-16 00:52:58 +0100

    Debugging an issue where static-menu background-image was squished at the start of dynamic menu display. Refactoring dynamic menu object and style.

--- assets/js/classes/DynamicMenu.model.js
+++ assets/js/classes/DynamicMenu.model.js
@@ -3,106 +3,87 @@ class DynamicMenu extends UserPanels {
         super();
 
         this._menuTriggerBtn = document.getElementById('dynamic-menu-button');
-        this._showcasePanel = document.getElementsByClassName('showcase-panel')[0];
-    }
+        this._bodyElt = document.getElementsByTagName('body')[0];
 
+        this._dynamicMenuContainer;
+        this._closeDynamicMenuLayer;
+        this._dynamicMenu;
+    }
+    
+    get dynamicMenuContainer() {
+        return this._dynamicMenuContainer;
+    }
+    
+    get dynamicMenu() {
+        return this._dynamicMenu;
+    }
+    
+    get closeDynamicMenuLayer() {
+        return this._closeDynamicMenuLayer;
+    }
+    
     get menuTriggerBtn() {
         return this._menuTriggerBtn;
     }
-
-    get showcasePanel() {
-        return this._showcasePanel;
+    
+    get bodyElt() {
+        return this._bodyElt;
     }
-
-    init() {
-        this.addMenuTriggerBtnListener();
+    
+    set dynamicMenuContainer(item) {
+        this._dynamicMenuContainer = item;
     }
-
-    addMenuTriggerBtnListener() {
-        this.menuTriggerBtn.addEventListener('click', () => {
-            this.setDynamicLayer();
-            this.setCloseMenuLayer();
-            this.triggerCorrectMenuButtons();
-            this.addCloseMenuEltsListener();
-        })
+    
+    set dynamicMenu(item) {
+        this._dynamicMenu = item;
     }
-
-    setDynamicLayer() {
-        const blurryLayer = document.createElement('div');
-        const dynamicMenuLayer = document.createElement('div');
-        const closeDynamicMenuLayer = document.createElement('div');
-        const dynamicMenu = document.createElement('div');
-
-        blurryLayer.classList.add('blurry-layer');
-        blurryLayer.style.opacity = 0;
-        dynamicMenuLayer.classList.add('dynamic-menu-layer', 'blue-bkgd');
-        closeDynamicMenuLayer.classList.add('close-menu-layer');
-        dynamicMenu.classList.add('dynamic-menu');
-
-        this.showcasePanel.appendChild(blurryLayer);
-        dynamicMenuLayer.appendChild(closeDynamicMenuLayer);
-        dynamicMenuLayer.appendChild(dynamicMenu);
-        this.showcasePanel.appendChild(dynamicMenuLayer);
-
-        this.fadeInItem(blurryLayer, 3000, 0.8);
-        this.slideInItem(dynamicMenuLayer, 2000);
+    
+    set closeDynamicMenuLayer(item) {
+        this._closeDynamicMenuLayer = item;
     }
-
-    slideInItem(item, timer) {
-        let intervalId = setInterval(() => {
-            let itemWidth = item.getBoundingClientRect().width;
-            if (item.getBoundingClientRect().width < 225) {
-                item.style.width = (itemWidth + 5 + 'px');
-            }
-            else {
-                clearInterval(intervalId);
-            }
-        }, timer/1000)
-    }
-
-    setCloseMenuLayer() {
-        const closeMenuLayer = document.getElementsByClassName('close-menu-layer')[0];
-        
-        const closeMenuBtn = document.createElement('button');
-        const closeMenuIcon = document.createElement('i');
-        
-        closeMenuIcon.classList.add('fa-solid', 'fa-xmark');
-        closeMenuIcon.id = 'close-menu-button';
-        
-        closeMenuBtn.appendChild(closeMenuIcon);
-        closeMenuLayer.appendChild(closeMenuBtn);
-    }
-
-    triggerCorrectMenuButtons() {
-        if (document.getElementById('logged')) {
-            this.addLoggedUserMenu();
-        }
-
-        else {
-            this.addDefaultMenu();
-        }
-    }
-
+    
     addCloseMenuEltsListener() {
-        const dynamicMenu = document.getElementsByClassName('dynamic-menu-layer')[0];
         const closeMenuBtn = document.getElementById('close-menu-button');
         const blurryLayer = document.getElementsByClassName('blurry-layer')[0];
         const clickableElts = [closeMenuBtn, blurryLayer];
 
         clickableElts.forEach((clickedElt) => {
             clickedElt.addEventListener('click', () => {
-                this.fadeOutItem(dynamicMenu, 1000);
-    
+                this.fadeOutItem(this.dynamicMenuContainer, 1000);
+                
                 setTimeout(() => {
-                    this.showcasePanel.removeChild(dynamicMenu);
-                    this.showcasePanel.removeChild(blurryLayer);
+                    this.bodyElt.removeChild(this.dynamicMenuContainer);
                 }, 100)
             })
         })
     }
-
+    
+    addDefaultMenu() {
+        const linkListElt = document.createElement('ul');
+        const loginListItem = document.createElement('li');
+        const contactListItem = document.createElement('li');
+        const loginLink = document.createElement('a');
+        const contactLink = document.createElement('a');
+        const loginSpan = document.createElement('span');
+        const contactSpan = document.createElement('span');
+        
+        loginLink.textContent = 'Connexion';
+        loginLink.href = 'index.php?page=login';
+        contactLink.textContent = 'Contact';
+        contactLink.href = 'index.php';
+        loginSpan.classList.add('menu-splitter');
+        contactSpan.classList.add('menu-splitter');
+        
+        loginListItem.appendChild(loginLink);
+        contactListItem.appendChild(contactLink);
+        linkListElt.appendChild(loginListItem);
+        linkListElt.appendChild(loginSpan);
+        linkListElt.appendChild(contactListItem);
+        linkListElt.appendChild(contactSpan);
+        this.dynamicMenu.appendChild(linkListElt)
+    }
+    
     addLoggedUserMenu() {
-        const dynamicMenu = document.getElementsByClassName('dynamic-menu')[0];
         const linkListElt = document.createElement('ul');
         const dashboardListItem = document.createElement('li');
         const contactListItem = document.createElement('li');
@@ -113,7 +94,6 @@ class DynamicMenu extends UserPanels {
         const dashboardSpan = document.createElement('span');
         const contactSpan = document.createElement('span');
         const logoutSpan = document.createElement('span');
-
         
         dashboardListItem.classList.add('dynamic-menu-item');
         contactListItem.classList.add('dynamic-menu-item');
@@ -131,42 +111,76 @@ class DynamicMenu extends UserPanels {
         dashboardListItem.appendChild(dashboardLink);
         contactListItem.appendChild(contactLink);
         logoutListItem.appendChild(logoutLink);
-        
         linkListElt.appendChild(dashboardListItem);
         linkListElt.appendChild(dashboardSpan);
         linkListElt.appendChild(contactListItem);
         linkListElt.appendChild(contactSpan);
         linkListElt.appendChild(logoutListItem);
         linkListElt.appendChild(logoutSpan);
-        
-        dynamicMenu.appendChild(linkListElt)
+        this.dynamicMenu.appendChild(linkListElt)
     }
-
-    addDefaultMenu() {
-        const dynamicMenu = document.getElementsByClassName('dynamic-menu')[0];
-        const linkListElt = document.createElement('ul');
-        const loginListItem = document.createElement('li');
-        const contactListItem = document.createElement('li');
-        const loginLink = document.createElement('a');
-        const contactLink = document.createElement('a');
-        const loginSpan = document.createElement('span');
-        const contactSpan = document.createElement('span');
-
-        loginLink.textContent = 'Connexion';
-        loginLink.href = 'index.php?page=login';
-        contactLink.textContent = 'Contact';
-        contactLink.href = 'index.php';
-        loginSpan.classList.add('menu-splitter');
-        contactSpan.classList.add('menu-splitter');
+    
+    addMenuTriggerBtnListener() {
+        this.menuTriggerBtn.addEventListener('click', () => {
+            this.setDynamicLayer();
+            this.setCloseMenuLayer();
+            this.triggerCorrectMenuButtons();
+            this.addCloseMenuEltsListener();
+        })
+    }
+    
+    init() {
+        this.addMenuTriggerBtnListener();
+    }
+    
+    setCloseMenuLayer() {
+        const closeMenuBtn = document.createElement('button');
+        const closeMenuIcon = document.createElement('i');
         
-        loginListItem.appendChild(loginLink);
-        contactListItem.appendChild(contactLink);
+        closeMenuIcon.classList.add('fa-solid', 'fa-xmark');
+        closeMenuIcon.id = 'close-menu-button';
         
-        linkListElt.appendChild(loginListItem);
-        linkListElt.appendChild(loginSpan);
-        linkListElt.appendChild(contactListItem);
-        linkListElt.appendChild(contactSpan);
+        closeMenuBtn.appendChild(closeMenuIcon);
+        this.closeDynamicMenuLayer.appendChild(closeMenuBtn);
+    }
+    
+    setDynamicLayer() {
+        const dynamicMenuContainer = document.createElement('div');
+        const blurryLayer = document.createElement('div');
+        const dynamicMenuLayer = document.createElement('div');
+        const closeDynamicMenuLayer = document.createElement('div');
+        const dynamicMenu = document.createElement('div');
         
-        dynamicMenu.appendChild(linkListElt)
+        dynamicMenuContainer.id = 'dynamic-menu-container';
+        blurryLayer.classList.add('blurry-layer');
+        blurryLayer.style.opacity = 0;
+        dynamicMenuLayer.classList.add('dynamic-menu-layer', 'blue-bkgd');
+        closeDynamicMenuLayer.classList.add('close-menu-layer');
+        dynamicMenu.classList.add('dynamic-menu');
+        
+        this.closeDynamicMenuLayer = dynamicMenuLayer.appendChild(closeDynamicMenuLayer);
+        this.dynamicMenu = dynamicMenuLayer.appendChild(dynamicMenu);
+        dynamicMenuContainer.appendChild(blurryLayer);
+        dynamicMenuContainer.appendChild(dynamicMenuLayer);
+        this.dynamicMenuContainer = this.bodyElt.appendChild(dynamicMenuContainer);
+        
+        this.fadeInItem(blurryLayer, 3000, 0.8);
+        this.slideInItem(dynamicMenuLayer, 2000);
+    }
+    
+    slideInItem(item, timer) {
+        let intervalId = setInterval(() => {
+            let itemWidth = item.getBoundingClientRect().width;
+            if (item.getBoundingClientRect().width < 225) {
+                item.style.width = (itemWidth + 5 + 'px');
+            }
+            else {
+                clearInterval(intervalId);
+            }
+        }, timer/1000)
+    }
+    
+    triggerCorrectMenuButtons() {
+        document.getElementById('logged') ? this.addLoggedUserMenu() : this.addDefaultMenu();
     }
 }
